5f4e7164765ebd48badf1fba6ff19626cd2334d3
rabia zia nezami <rabianezami786@gmail.com>
2026-04-14 11:45:27 +0430

    use toolbar and set state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
+import { useState } from "react"
+import { useQuery } from "@tanstack/react-query"
 import Header from "./components/Header"
+import Toolbaar from "./components/Toolbar"
+import { fetchUsers } from "./api/users"
 
 
 function App() {
-  
+
+  const [search, setSearch] = useState("")
+
+  const { data = [], isLoading, isError, error, isFetching, refetch } = useQuery({
+    queryKey: ['users'],
+    queryFn: fetchUsers,
+
+  })
+
 
   return (
     <main className="min-h-screen bg-gradient-t0-br from-slate-100 via-white to-sky-100 px-4 py-10 text-slate-900">
-      <div>
-        <Header 
-        totalUsers={0} 
-        selectedCount={0}
-        isFetching={false} 
+      <div className="mx-auto max-w-7xl">
+        <Header
+          totalUsers={data.length}
+          selectedCount={0}
+          isFetching={isFetching}
+        />
+
+        <Toolbaar
+          search={search}
+          setSearch={setSearch}
+          onRefrech={refetch}
+          isFetching={isFetching}
         />
       </div>
     </main>
